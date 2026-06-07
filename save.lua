0ab2be40372b445435968e9ebbacd3a2f954e3e6
Ayaan Hafeez <m.ayaan.hafeez@gmail.com>
2026-06-08 01:35:32 +0500

    saving world room and item states

--- save.lua
+++ save.lua
@@ -1,3 +1,5 @@
+local world = require("world")
+
 local save_template = {
   current_room = true,
   previous_room = true,
@@ -6,9 +8,33 @@ local save_template = {
   unlocked = true,
   destroyed = true,
   elapsed = true,
+  rooms =true,
   command_count = true
 }
 
+local function add_world_items(state, filename)
+  local rooms = {}
+  for room_id in pairs(state.visited) do
+    local room = world.rooms[room_id]
+    rooms[room_id] = { items = {}}
+    if room.mode then rooms[room_id].mode = room.mode end
+    for filename, item in pairs(room.items) do
+      rooms[room_id].items[filename] = {
+        id = item.id,
+        filename = filename,
+        room = item.room,
+        copied = item.copied
+      }
+    end
+  end
+  state.rooms = rooms
+  return state
+end
+
+local function remove_world_items(state)
+  state.rooms = nil
+  return state
+end
 
 local function convert_state_to_string(state, template) --function taken from https://gist.github.com/justnom/9816256 and modified
   local state_string = "{"
@@ -44,11 +70,13 @@ end
 local M = {}
 
 function M.save_state(state, filename)
-  local state_string = convert_state_to_string(state, save_template)
+  local state_with_world = add_world_items(state, filename)
+  local state_string = convert_state_to_string(state_with_world, save_template)
   local successs, message = love.filesystem.write(filename, state_string)
   if not successs then
     print(message)
   end
+  remove_world_items(state)
 end
 
 function M.load_state(filename)
