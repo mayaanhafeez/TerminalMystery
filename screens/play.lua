0c1da79431f79d0fa7da8a5df1379428bfb3029e
Ayaan Hafeez <m.ayaan.hafeez@gmail.com>
2026-06-14 01:34:59 +0500

    refactored play screen to match lua convention and updated new button position logic to only trigger if screen size has changed

--- screens/play.lua
+++ screens/play.lua
@@ -1,24 +1,24 @@
 Screen = require("screen")
 Render = require("render")
 
-local ASPECT_RATIO = 0.2
-local PADDING = 80
-local CENTER= {x = 250, y = 250}
-local SCREEN_SIZE = {w = 500, h = 500}
-local BUTTON_SIZE = {w = ((24/9)*SCREEN_SIZE.w)*ASPECT_RATIO, h = SCREEN_SIZE.h*ASPECT_RATIO}
+local aspect_ratio = 0.2
+local padding = 80
+local center= {x = 250, y = 250}
+local screen_size = {w = 500, h = 500}
+local button_size = {w = ((24/9)*screen_size.w)*aspect_ratio, h = screen_size.h*aspect_ratio}
 
-local function get_button_position(wanted_x, wanted_y, button_size)
+local function get_button_position(wanted_x, wanted_y, this_button_size)
   -- converts given center position to top left corner
-  local actual_position = {x = wanted_x - button_size.w/2, y = wanted_y - button_size.h/2}
+  local actual_position = {x = wanted_x - this_button_size.w/2, y = wanted_y - this_button_size.h/2}
   return actual_position
 end
 
 local function button_selected()
   local x,y = love.mouse.getPosition()
   for _, my_button in ipairs(M.buttons) do
-    local button_size = my_button.size
+    local this_button_size = my_button.size
     local button_position = my_button.position
-    if x > button_position.x and x< (button_position.x + button_size.w) and y > button_position.y and y<(button_position.y + button_size.h) then
+    if x > button_position.x and x< (button_position.x + this_button_size.w) and y > button_position.y and y<(button_position.y + this_button_size.h) then
       return my_button
     end
   end
@@ -26,14 +26,13 @@ local function button_selected()
 end
 
 local function draw_button(button)
-  local button_size = BUTTON_SIZE
-  local x, y = button.position
   local button_position = button.position
+  local this_button_size = button.size
   love.graphics.setColor(1,1,1)
-  love.graphics.rectangle("fill", button_position.x, button_position.y, button_size.w, button_size.h, 5)
+  love.graphics.rectangle("fill", button_position.x, button_position.y, this_button_size.w, this_button_size.h, 5)
   local label = button.label
   local font = love.graphics.getFont()
-  local text_position = {x = button_position.x + (button_size.w - font:getWidth(label)) /2, y= button_position.y + (button_size.h - font:getHeight()) /2}
+  local text_position = {x = button_position.x + (this_button_size.w - font:getWidth(label)) /2, y= button_position.y + (this_button_size.h - font:getHeight()) /2}
   love.graphics.setColor(0,0,0)
   love.graphics.print(label, text_position.x, text_position.y)
 end
@@ -44,9 +43,12 @@ M.buttons = {}
 
 function M.draw()
   --get screen middle
-  W, H = love.graphics.getDimensions()
-  CENTER = {x = W/2, y = H/2}
-  M.load_buttons()
+  local cw,ch = love.graphics.getDimensions()
+  if screen_size.w~=cw or screen_size.h~=ch then
+    screen_size.w, screen_size.h = cw,ch
+    center = {x = screen_size.w/2, y = screen_size.h/2}
+    M.load_buttons()
+  end
   love.graphics.clear(0,0,0,1)
   for _, button in ipairs(M.buttons) do
     draw_button(button)
@@ -62,8 +64,8 @@ end
 
 function M.load_buttons()
   M.buttons = {
-    {label = "play", size = BUTTON_SIZE, position = get_button_position(CENTER.x, CENTER.y, BUTTON_SIZE), action = function() Screen.set("game") end},
-    {label = "exit", size = BUTTON_SIZE, position = get_button_position(CENTER.x, CENTER.y + PADDING + (BUTTON_SIZE.h/2) , BUTTON_SIZE), action = function() love.event.quit() end},
+    {label = "play", size = button_size, position = get_button_position(center.x, center.y, button_size), action = function() Screen.set("game") end},
+    {label = "exit", size = button_size, position = get_button_position(center.x, center.y + padding + (button_size.h/2) , button_size), action = function() love.event.quit() end},
   }
 end
 
