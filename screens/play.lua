4c038b52aebbc4002f10d857f0801d3544ec51ec
Ayaan Hafeez <m.ayaan.hafeez@gmail.com>
2026-07-13 02:04:02 +0500

    button color changes

--- screens/play.lua
+++ screens/play.lua
@@ -24,6 +24,56 @@ local function get_button_position(wanted_x, wanted_y, this_button_size)
   return actual_position
 end
 
+local function draw_button_border(x, y, w, h)
+	local B = 11
+	local u = 2 -- pixel unit for outlines / bevel
+	local edge = { 0.10, 0.08, 0.06 }
+	local body = { 0.42, 0.30, 0.18 }
+	local hi = { 0.64, 0.48, 0.28 }
+	local sh = { 0.24, 0.16, 0.09 }
+
+	-- body bands framing the opening (top / bottom / left / right)
+	love.graphics.setColor(body)
+	love.graphics.rectangle("fill", x, y, w, B)
+	love.graphics.rectangle("fill", x, y + h - B, w, B)
+	love.graphics.rectangle("fill", x, y, B, h)
+	love.graphics.rectangle("fill", x + w - B, y, B, h)
+
+	-- outer dark outline
+	love.graphics.setColor(edge)
+	love.graphics.rectangle("fill", x, y, w, u)
+	love.graphics.rectangle("fill", x, y + h - u, w, u)
+	love.graphics.rectangle("fill", x, y, u, h)
+	love.graphics.rectangle("fill", x + w - u, y, u, h)
+
+	-- bevel: highlight along top + left, shadow along bottom + right
+	love.graphics.setColor(hi)
+	love.graphics.rectangle("fill", x + u, y + u, w - 2 * u, u)
+	love.graphics.rectangle("fill", x + u, y + u, u, h - 2 * u)
+	love.graphics.setColor(sh)
+	love.graphics.rectangle("fill", x + u, y + h - 2 * u, w - 2 * u, u)
+	love.graphics.rectangle("fill", x + w - 2 * u, y + u, u, h - 2 * u)
+
+	-- inner dark outline around the opening
+	love.graphics.setColor(edge)
+	local ix, iy, iw, ih = x + B - u, y + B - u, w - 2 * (B - u), h - 2 * (B - u)
+	love.graphics.rectangle("fill", ix, iy, iw, u)
+	love.graphics.rectangle("fill", ix, iy + ih - u, iw, u)
+	love.graphics.rectangle("fill", ix, iy, u, ih)
+	love.graphics.rectangle("fill", ix + iw - u, iy, u, ih)
+
+	-- corner studs
+	local s = 5
+	love.graphics.setColor(hi)
+	for _, c in ipairs({ { x + u + 1, y + u + 1 }, { x + w - u - 1 - s, y + u + 1 },
+		{ x + u + 1, y + h - u - 1 - s }, { x + w - u - 1 - s, y + h - u - 1 - s } }) do
+		love.graphics.rectangle("fill", c[1], c[2], s, s)
+		love.graphics.setColor(edge)
+		love.graphics.rectangle("fill", c[1] + s - 1, c[2] + s - 1, 1, 1)
+		love.graphics.setColor(hi)
+	end
+end
+
 local function button_selected()
   local x,y = love.mouse.getPosition()
   for _, my_button in ipairs(M.buttons) do
@@ -42,18 +92,21 @@ local function draw_button(button, color, this_button_size, button_position)
   local color2 = color[2]
   local color3 = color[3]
   love.graphics.setColor(color1,color2,color3)
-  love.graphics.rectangle("fill", button_position.x, button_position.y, this_button_size.w, this_button_size.h, 5)
+  love.graphics.rectangle("fill", button_position.x, button_position.y, this_button_size.w, this_button_size.h)
   local label = button.label
   local font = love.graphics.getFont()
   local text_position = {x = button_position.x + (this_button_size.w - font:getWidth(label)) /2, y= button_position.y + (this_button_size.h - font:getHeight()) /2}
-  love.graphics.setColor(0,0,0)
+  love.graphics.setColor(43/255,36/255,25/255)
   love.graphics.print(label, text_position.x, text_position.y)
+  draw_button_border(button_position.x, button_position.y, this_button_size.w, this_button_size.h)
 end
 
 M.buttons = {}
 
 function M.draw()
   --get screen middle
+  local button_color = {0.91, 0.87, 0.78}
+  local hover_color = {221/255, 208/255, 180/255}
   local cw,ch = love.graphics.getDimensions()
   if screen_size.w~=cw or screen_size.h~=ch then
     screen_size.w, screen_size.h = cw,ch
@@ -80,13 +133,14 @@ function M.draw()
     if (button_selected()) then
       if (button.label == button_selected().label) then
         this_button_size = {w =this_button_size.w * 1.03, h =this_button_size.h *1.03}
-        draw_button(button,{0.67,0.67,0.67},this_button_size, button.position)
+        draw_button(button,hover_color,this_button_size, button.position)
         this_button_size = {w = button.size.w,h= button.size.h}
       else
-        draw_button(button, {1,1,1}, this_button_size, button.position)
+
+        draw_button(button, button_color, this_button_size, button.position)
       end
     else
-      draw_button(button, {1,1,1}, this_button_size, button.position)
+      draw_button(button, button_color, this_button_size, button.position)
     end
   end
 end
--- screens/play_menu.lua
+++ screens/play_menu.lua
@@ -15,6 +15,55 @@ end
 
 local M = {}
 
+local function draw_button_border(x, y, w, h)
+	local B = 11
+	local u = 2 -- pixel unit for outlines / bevel
+	local edge = { 0.10, 0.08, 0.06 }
+	local body = { 0.42, 0.30, 0.18 }
+	local hi = { 0.64, 0.48, 0.28 }
+	local sh = { 0.24, 0.16, 0.09 }
+
+	-- body bands framing the opening (top / bottom / left / right)
+	love.graphics.setColor(body)
+	love.graphics.rectangle("fill", x, y, w, B)
+	love.graphics.rectangle("fill", x, y + h - B, w, B)
+	love.graphics.rectangle("fill", x, y, B, h)
+	love.graphics.rectangle("fill", x + w - B, y, B, h)
+
+	-- outer dark outline
+	love.graphics.setColor(edge)
+	love.graphics.rectangle("fill", x, y, w, u)
+	love.graphics.rectangle("fill", x, y + h - u, w, u)
+	love.graphics.rectangle("fill", x, y, u, h)
+	love.graphics.rectangle("fill", x + w - u, y, u, h)
+
+	-- bevel: highlight along top + left, shadow along bottom + right
+	love.graphics.setColor(hi)
+	love.graphics.rectangle("fill", x + u, y + u, w - 2 * u, u)
+	love.graphics.rectangle("fill", x + u, y + u, u, h - 2 * u)
+	love.graphics.setColor(sh)
+	love.graphics.rectangle("fill", x + u, y + h - 2 * u, w - 2 * u, u)
+	love.graphics.rectangle("fill", x + w - 2 * u, y + u, u, h - 2 * u)
+
+	-- inner dark outline around the opening
+	love.graphics.setColor(edge)
+	local ix, iy, iw, ih = x + B - u, y + B - u, w - 2 * (B - u), h - 2 * (B - u)
+	love.graphics.rectangle("fill", ix, iy, iw, u)
+	love.graphics.rectangle("fill", ix, iy + ih - u, iw, u)
+	love.graphics.rectangle("fill", ix, iy, u, ih)
+	love.graphics.rectangle("fill", ix + iw - u, iy, u, ih)
+
+	-- corner studs
+	local s = 5
+	love.graphics.setColor(hi)
+	for _, c in ipairs({ { x + u + 1, y + u + 1 }, { x + w - u - 1 - s, y + u + 1 },
+		{ x + u + 1, y + h - u - 1 - s }, { x + w - u - 1 - s, y + h - u - 1 - s } }) do
+		love.graphics.rectangle("fill", c[1], c[2], s, s)
+		love.graphics.setColor(edge)
+		love.graphics.rectangle("fill", c[1] + s - 1, c[2] + s - 1, 1, 1)
+		love.graphics.setColor(hi)
+	end
+end
 
 local function load_from_save()
   save_data = Save.load_state("save_data.txt")
@@ -64,6 +113,7 @@ local function draw_button(button, color, this_button_size, button_position)
   local text_position = {x = button_position.x + (this_button_size.w - font:getWidth(label)) /2, y= button_position.y + (this_button_size.h - font:getHeight()) /2}
   love.graphics.setColor(0,0,0)
   love.graphics.print(label, text_position.x, text_position.y)
+  draw_button_border(button_position.x, button_position.y, this_button_size.w, this_button_size.h)
 end
 
 M.buttons = {}
@@ -74,6 +124,8 @@ end
 
 function M.draw()
   --get screen middle
+  local button_color = {0.91, 0.87, 0.78}
+  local hover_color = {221/255, 208/255, 180/255}
   local cw,ch = love.graphics.getDimensions()
   if screen_size.w~=cw or screen_size.h~=ch then
     screen_size.w, screen_size.h = cw,ch
@@ -86,13 +138,13 @@ function M.draw()
     if (button_selected()) then
       if (button.label == button_selected().label) then
         this_button_size = {w =this_button_size.w * 1.03, h =this_button_size.h *1.03}
-        draw_button(button,{0.67,0.67,0.67},this_button_size, button.position)
+        draw_button(button,hover_color,this_button_size, button.position)
         this_button_size = {w = button.size.w,h= button.size.h}
       else
-        draw_button(button, {1,1,1}, this_button_size, button.position)
+        draw_button(button, button_color, this_button_size, button.position)
       end
     else
-      draw_button(button, {1,1,1}, this_button_size, button.position)
+      draw_button(button, button_color, this_button_size, button.position)
     end
   end
 end
